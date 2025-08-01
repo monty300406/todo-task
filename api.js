c2b961dd1d85e906b5ce05631e83abbc7518ff62
Santiago Montoya <Santy30040626@hotmail.com>
2025-07-31 20:47:12 -0500

    CRUD

--- api.js
+++ api.js
@@ -71,4 +71,43 @@ router.post("/create-task", function (req, res) {
     });
 });
 
+router.get("/all-tasks", function (req, res) {
+  TaskModel.find()
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Internal error\n");
+    });
+});
+
+router.post("/update-task", function (req, res) {
+  TaskModel.updateOne(
+    { TaskId: req.body.TaskId },
+    {
+      Name: req.body.Name,
+      Deadline: req.body.Deadline,
+    }
+  )
+    .then((data) => {
+      res.status(200).send("OK\n");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Internal error\n");
+    });
+});
+
+router.delete("/delete-task", function (req, res) {
+  TaskModel.deleteOne({ TaskId: req.body.TaskId })
+    .then((data) => {
+      res.status(200).send("OK\n");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Internal error\n");
+    });
+});
+
 module.exports = router;
